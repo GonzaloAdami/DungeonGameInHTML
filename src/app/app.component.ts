669700232a264c4d7f5a7a2f4d7aed6123641519
Gonzalo <gonzaloadamie@gmail.com>
2023-08-25 21:06:35 -0300

    interface sin inventario

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  inventario = false; // Inicialmente, el div se mostrará
   title = 'DungeonHTML';
+  hp = 255;
+  mp = 300;
+  tp = 100;
+  enemyHP = 2255;
+  personaje: {hpPerson: number, mpPerson: number, tpPerson: number, dmgPerson: number, armPerson: number};
+enemy: {enemyHP: number, enemyDMG: number, enemyARM: number};
+  constructor() {
+    this.personaje = {
+      hpPerson: 255,
+      mpPerson: 300,
+      tpPerson: 100,
+      dmgPerson: 100,
+      armPerson: 100,
+    };
+    this.enemy = {
+      enemyHP: 2255,
+      enemyDMG: 300,
+      enemyARM: 100,
+    }
+}
+
+
+  abrirInventario() {
+    this.inventario = !this.inventario; // Alternar entre mostrar y ocultar
+  }
 }
